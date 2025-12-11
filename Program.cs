315432aa3bf489a9b5b9487272491c08bc69e8c3
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-11 10:55:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ForLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("\n\n");
+            Console.Write("Tärnidest tehtud kolmnurk e poolik püramiid:\n");
+            Console.Write("------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Sisesta ridade arv : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
